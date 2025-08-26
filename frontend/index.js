3afa75bb8ed0101ceb832cab1bbbb2569c135a9a
darshil-nakrani <darshilnakrani296@gmail.com>
2025-08-26 18:45:23 +0530

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const bodyParser = require('body-parser');
 
 const app = express();
-const BACKEND_URL = 'http://3.110.43.178:9000';
+const BACKEND_URL = 'http://3.109.208.106:9000';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -57,6 +57,7 @@ const PORT = 8000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Frontend server running at http://localhost:${PORT}`);
 });
+
 
 
 
